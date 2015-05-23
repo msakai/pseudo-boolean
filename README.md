41bb22fe5be52ffa2de265f8ecaff29e87e7bb50
Masahiro Sakai <masahiro.sakai@gmail.com>
2015-05-24 00:02:16 +0900

    initial import

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pseudo-boolean
 Haskell library for parsing/generating OPB/WBO files used in pseudo boolean competition.
+
+[![Build Status](https://secure.travis-ci.org/msakai/pseudo-boolean.png?branch=master)](http://travis-ci.org/msakai/pseudo-boolean) [![Coverage Status](https://coveralls.io/repos/msakai/pseudo-boolean/badge.svg)](https://coveralls.io/r/msakai/pseudo-boolean) [![Hackage](https://budueba.com/hackage/pseudo-boolean)](https://hackage.haskell.org/package/pseudo-boolean)
---
+++ src/Data/PseudoBoolean.hs
@@ -0,0 +1,72 @@
+{-# LANGUAGE BangPatterns #-}
+{-# OPTIONS_GHC -Wall #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.PseudoBoolean
+-- Copyright   :  (c) Masahiro Sakai 2011-2015
+-- License     :  BSD-style
+-- 
+-- Maintainer  :  masahiro.sakai@gmail.com
+-- Portability :  non-portable (BangPatterns)
+--
+-- A library for parsing\/generating OPB\/WBO files used in pseudo boolean competition.
+-- 
+-- References:
+--
+-- * Input/Output Format and Solver Requirements for the Competitions of
+--   Pseudo-Boolean Solvers
+--   <http://www.cril.univ-artois.fr/PB11/format.pdf>
+--
+-----------------------------------------------------------------------------
+
+module Data.PseudoBoolean
+  (
+  -- * Abstract Syntax
+    Formula (..)
+  , Constraint
+  , Op (..)
+  , SoftFormula (..)
+  , SoftConstraint
+  , Sum
+  , WeightedTerm
+  , Term
+  , Lit
+  , Var
+
+  -- * Parsing OPB files
+  -- $ParsingOPB
+  , parseOPBString
+  , parseOPBByteString
+  , parseOPBFile
+
+  -- * Parsing WBO files
+  -- $ParsingWBO
+  , parseWBOString
+  , parseWBOByteString
+  , parseWBOFile
+
+  -- * Generating OPB files
+  , toOPBString
+  , toOPBByteString
+  , writeOPBFile
+  , hPutOPB
+
+  -- * Generating WBO files
+  , toWBOString
+  , toWBOByteString
+  , writeWBOFile
+  , hPutWBO
+  ) where
+
+import Data.PseudoBoolean.Parsec
+import Data.PseudoBoolean.Types
+import Data.PseudoBoolean.Builder
+import Data.PseudoBoolean.ByteStringBuilder as ByteStringBuilder
+
+-- $ParsingOPB
+-- These functions are based on Parsec.
+-- If you want faster parser, you can also use "Data.PseudoBoolean.Attoparsec" module.
+
+-- $ParsingWBO
+-- These functions are based on Parsec. 
+-- If you want faster parser, you can also use "Data.PseudoBoolean.Attoparsec" module.
